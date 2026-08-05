6887364ea2f296b24fcd458d336710683faf667c
Priyam <pradumnpathak87@gmail.com>
2026-08-05 16:32:34 +0530

    fix

--- src/components/oneTone.jsx
+++ src/components/oneTone.jsx
@@ -493,7 +493,7 @@ export default function WallbooksChat() {
     const initSocket = () => {
       console.log('[Socket] Initialising for user:', currentUserId);
 
-      socket = io(`${BACKEND_URL}`, {
+      socket = io(`${BACKEND_URL}/private`, {
         transports: ['polling', 'websocket'],
         reconnection: true,
         reconnectionAttempts: 20,
